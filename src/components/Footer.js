c4ad8df9cf77b42407614f2bab048e2f8a6b8065
Yash Ranipa <ranipayash01@student.fdu.edu>
2022-06-14 14:10:59 -0700

    Added Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="footer py-4 border-top text-center mt-4 text-light">
+      Made by Yash Ranipa
+    </footer>
+  );
+};
